4a9029b400cf3073f277b7e037f0f25f0c7fddec
Pushkar <pushkarcdn@gmail.com>
2025-01-15 20:27:52 +0545

    added conf env for vercel

--- server-v.js
+++ server-v.js
@@ -1,11 +1,13 @@
 const app = require("./app");
 const http = require("http");
 
+const config = require("./config/config");
+
 // Vercel expects an HTTP server to be used for Node.js deployments
 const server = http.createServer(app);
 
 // Port will be set by Vercel's environment variable
-const port = process.env.PORT || 3000;
+const port = config.port || 3000;
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
