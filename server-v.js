78b1c8a9bb58e9eb72ecd50f0c1f592aa056470a
Pushkar <pushkarcdn@gmail.com>
2025-01-15 20:29:43 +0545

    env

--- server-v.js
+++ server-v.js
@@ -1,13 +1,13 @@
 const app = require("./app");
 const http = require("http");
 
-const config = require("./config/config");
+require("dotenv").config({ path: `.env` });
 
 // Vercel expects an HTTP server to be used for Node.js deployments
 const server = http.createServer(app);
 
 // Port will be set by Vercel's environment variable
-const port = config.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
