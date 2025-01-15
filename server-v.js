f593c100bdca077bacad604bbb9a304fcf15e6e0
Pushkar <pushkarcdn@gmail.com>
2025-01-15 20:45:39 +0545

    envsss

--- server-v.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app"); // Your express app
-const { port = 3000 } = process.env; // Use PORT from environment, fallback to 3000
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
